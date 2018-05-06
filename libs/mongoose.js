0c474c0439e93a2a4d6b49a9a93b1eafe475b940
MrHrat <hrat1995@gmail.com>
2018-05-06 16:55:49 +0300

    Git clean report

--- libs/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var myconfig = require('./myconfig');
-
-mongoose.set('debug', true);
-
-mongoose.connect(myconfig.mongoose.uri), myconfig.mongoose.options;
-
-module.exports = mongoose;
--- libs/sessionStore.js
+++
@@ -1,8 +0,0 @@
-
-var session = require('express-session');
-var mongoose = require('../libs/mongoose');
-var mongoStore = require('connect-mongo')(session);
-
-var sessionStore = new mongoStore({mongooseConnection: mongoose.connection});
-
-module.exports = sessionStore;
